c85cdf30c13549e6d0d1377bf5bda41f1b52c7d5
Jamie Jennings <jamiejennings@users.noreply.github.com>
2018-05-13 08:06:38 -0400

    Now installing beta-10

--- rosie.rb
+++ rosie.rb
@@ -4,7 +4,7 @@
 class Rosie < Formula
   desc "The Rosie Project's pattern language and pattern matching engine"
   homepage "http://rosie-lang.org"
-  url "https://github.com/jamiejennings/rosie-pattern-language.git", :tag => "v1.0.0-beta-9"
+  url "https://github.com/jamiejennings/rosie-pattern-language.git", :tag => "v1.0.0-beta-10"
   version "1.0.0-beta"
   head "https://github.com/jamiejennings/rosie-pattern-language.git", :branch => "master"
   #  sha256 ""
